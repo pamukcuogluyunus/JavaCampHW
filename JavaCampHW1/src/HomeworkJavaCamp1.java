529fdb9445c60c2be9e5ff72c9783e81c197dcc3
yunusemrepamukcuoglu <yunus82716@ogr.duzce.edu.tr>
2022-10-15 19:38:22 +0300

    JavaCampHW
    projects

---
+++ JavaCampHW1/src/HomeworkJavaCamp1.java
@@ -0,0 +1,12 @@
+
+public class HomeworkJavaCamp1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Merhaba Java");
+		System.out.println("Merhaba Java 2");
+
+	}
+
+}
---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,18 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+      
+		short sayi = 12;
+		sayi = 32767;
+		
+		
+		char karakter = 'A';
+		boolean dogruMu=false;
+		
+		
+		
+	}
+
+}
